9a013fef810558053fb4c9cd86fc154a0b59c2ff
Aldas25 <aldas.lenksas@gmail.com>
2021-04-02 12:22:13 +0300

    Reorganized folder structure

---
+++ source/auth_example.py
@@ -0,0 +1,6 @@
+trello_auth = {
+    'key': '<trello api key>',
+    'token': '<trello api token>',
+    'board_id': '<id of the board where the contest list is located>',
+    'contest_list_id': '<id of the list for contests>'
+}
